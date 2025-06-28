fd62d62fe3d5ecd104a936e0b78b6a07ca401c25
AshishPandey12358 <pashishk5@gmail.com>
2025-06-28 21:48:44 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -60,3 +60,5 @@ if(process.env.NODE_ENV !== "production"){
     const PORT = process.env.PORT || 5000;
     server.listen(PORT, ()=>console.log("Server is Running on PORT: "+PORT));
 }
+
+export default server
